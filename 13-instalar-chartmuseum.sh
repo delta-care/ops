53d70271f3d24eeb8005c0db18756f0263122301
David Carvalho <jdavidsilv@gmail.com>
2020-12-20 17:52:00 +0000

    alteração repositório chartm

--- 13-instalar-chartmuseum.sh
+++ 13-instalar-chartmuseum.sh
@@ -2,7 +2,8 @@
 # De: https://artifacthub.io/packages/helm/chartmuseum/chartmuseum
 # Em: 20/12/20
 
-helm install chartmuseum -f 13-instalar-chartmuseum-conf.yaml stable/chartmuseum --namespace ops
+helm repo add chartmuseum https://chartmuseum.github.io/charts
+helm install local -f 13-instalar-chartmuseum-conf.yaml chartmuseum/chartmuseum --namespace ops
 #export NODE_PORT=$(kubectl get --namespace ops -o jsonpath="{.spec.ports[0].nodePort}" services chartmuseum-chartmuseum)
 #export NODE_IP=$(kubectl get nodes --namespace ops -o jsonpath="{.items[0].status.addresses[0].address}")
 #helm repo add local http://$NODE_IP:$NODE_PORT/
