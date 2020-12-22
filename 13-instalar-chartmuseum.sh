634f22963ff47ee3c0a40dbed29e8181745209ff
David Carvalho <jdavidsilv@gmail.com>
2020-12-22 16:01:58 +0000

    alteração instalação chatmuseum

--- 13-instalar-chartmuseum.sh
+++ 13-instalar-chartmuseum.sh
@@ -3,7 +3,7 @@
 # Em: 20/12/20
 
 helm repo add chartmuseum https://chartmuseum.github.io/charts
-helm install local -f 13-instalar-chartmuseum-conf.yaml chartmuseum/chartmuseum --namespace ops
-#export NODE_PORT=$(kubectl get --namespace ops -o jsonpath="{.spec.ports[0].nodePort}" services chartmuseum-chartmuseum)
-#export NODE_IP=$(kubectl get nodes --namespace ops -o jsonpath="{.items[0].status.addresses[0].address}")
-#helm repo add local http://$NODE_IP:$NODE_PORT/
+helm install deltacare -f 13-instalar-chartmuseum-conf.yaml chartmuseum/chartmuseum --namespace ops
+export NODE_PORT=$(kubectl get --namespace ops -o jsonpath="{.spec.ports[0].nodePort}" services deltacare-chartmuseum)
+export NODE_IP=$(kubectl get nodes --namespace ops -o jsonpath="{.items[0].status.addresses[0].address}")
+helm repo add deltacare http://$NODE_IP:$NODE_PORT/
