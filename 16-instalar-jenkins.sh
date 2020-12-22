d861e8d08141c08ca02a0b6b739b41249dc7e0cf
David Carvalho <jdavidsilv@gmail.com>
2020-12-22 12:06:48 +0000

    atualização instalar jenkins

--- 16-instalar-jenkins.sh
+++ 16-instalar-jenkins.sh
@@ -3,3 +3,6 @@
 # Em: 20/12/20
 
 kubectl create -f 16-instalar-jenkins.yaml -n ops
+
+# configurar kubernetes no jenkins https://youtu.be/OfoAYVi1YcU?t=243
+# instalar plugin Pipeline Utility Steps
