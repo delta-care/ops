39eac4062d2f0f8c3439531b7f1d981f78476c6c
David Carvalho <jdavidsilv@gmail.com>
2021-02-15 18:56:52 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 provider "aws" {
     region = "us-east-2"
-    access_key = "AKIAISWJGNS3VY6HQQVA"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
 resource "aws_key_pair" "k8s-kp" {
     key_name   = "lenovod"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBaRKDwH9oEMJCRiMs4abSl9T6oO9PDM519omJDltj9rFiewMINILqdlIQA3hDEEbshD1tjgdRCnrofwkjGtJbWS1LYFnMWHILx2XXRtUNwc4jGbUWu54CyvWf9Vyz+EE8Ly3coP61F5n3AvOs+hkpBvbcn1MNrC5CTWvBxpYj8xbBvUub1qwMtJDwwAV2Car5efuPH17rV/7Up2Qluf4wYLqfkOpJnJ3zhAluWBWLtn3rHjkXdWF0NR2KFS2d/XDtU3g8XCyek9h1XGSPze7K+MM9vhbighfJacy3o+lIWqE+8pwSeem//9ZOPdgZsP3M40gexUIJ7tt051g/03/5GPGCkAuvm8NfagXeQfY7fxh9yH1v0FWuoD2ZYz3Gq7ap0u00RRMWsN7ZgvpdEuLXjUJWELHZ89DuJDnyt5B08AXr/L1oRrRd1nszKteiHJ7yS2UjPhJbMuy8vY1Mkwqqr74Ze9R0j9zU7hHtOBZmoRw+2thgHHYfg918U3x7w1c= lenovo@DESKTOP-F30NHET"
+    public_key = ""
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
