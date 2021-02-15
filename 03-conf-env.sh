f2b9f2c6bafe5240fb5c5ea85a7cac5b8c58ba39
David Carvalho <jdavidsilv@gmail.com>
2021-02-15 18:57:49 -0300

    Update 03-conf-env.sh

--- 03-conf-env.sh
+++ 03-conf-env.sh
@@ -4,8 +4,8 @@
 # https://docs.ansible.com/ansible/2.5/user_guide/intro_dynamic_inventory.html#example-aws-ec2-external-inventory-script
 export AWS_ACCESS_KEY_ID=''
 export AWS_SECRET_ACCESS_KEY=''
-#pip install boto3
-#pip install botocore
+pip install boto3
+pip install botocore
 
 # install packages
 ansible-inventory -i ./ansible/inventory/aws_ec2.yaml --graph
